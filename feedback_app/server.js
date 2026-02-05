a6f2de775464249aa3205e369eb8ee473a7c5cf1
Abhishek <abhishekshivajipisal@gmail.com>
2026-02-05 11:31:56 +0000

    Server.js File Modified along With Dockerfile

--- feedback_app/server.js
+++ feedback_app/server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
